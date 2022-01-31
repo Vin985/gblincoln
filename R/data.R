fd97c5de6f7cc7fd26c8f7a04bf3142800a6f909
Vin985 <sylvain.christin@gmail.com>
2022-01-31 16:56:22 -0400

    convenience functions for listing locations

--- R/data.R
+++ R/data.R
@@ -48,7 +48,76 @@
 #'}
 "gb_banding"
 
+get_species_data <- function(species_name, db_dtype = "banding") {
+  if (db_type == "banding") {
+    db = gb_banding
+  }
+  return(db[db$SPEC == species_name, ])
+}
 
-get_species_data <- function(species_name){
-  return(gb_banding[, gb_banding$SPEC == species_name])
+#' @title List the locations available for a species
+#' @description This is a convenience function to list all available locations
+#' for a given species. By default, a data frame is returned with available
+#' countries, states and flyways
+#' @param spec The 4 letters short code for the desired species
+#' (as found in the SPEC column of the gb_banding dataset)
+#' @param columns PARAM_DESCRIPTION, Default: c("country_name", "state_name", "flyway")
+#' @param db_type PARAM_DESCRIPTION, Default: 'banding'
+#' @param sort_by PARAM_DESCRIPTION, Default: NULL
+#' @return If columns contains more than one element, a data frame with all
+#' available combinations of the selected columns.
+#' If columns contains only one element, a sorted vector of the unique values
+#' of the selected column
+#' @details DETAILS
+#' @examples
+#' get_species_location("ATBR")
+#'
+#' get_species_location("ATBR", "country_name")
+#' # is equivalent to:
+#' get_species_countries("ATBR")
+#'
+#' @rdname get_species_locations
+#' @export
+get_species_locations <-
+  function(spec,
+           columns = c("country_name", "state_name", "flyway"),
+           db_type = "banding",
+           sort_by = NULL) {
+    if (db_type == "banding") {
+      db = gb_banding
+    }
+    if (length(columns) == 1) {
+      res = sort(unique(db[db$SPEC == spec, columns]))
+    } else {
+      res = db[db$SPEC == species_name, columns] %>% distinct()
+      if (!is.null(sort_by)) {
+        print(sort_by)
+        res = res %>% arrange(.data[[sort_by]])
+      }
+    }
+    return(res)
+  }
+
+#' @inheritParams get_species_locations
+#' @describeIn get_species_locations Convenience functions to list
+#'             all available countries for the given species.
+#' @export
+get_species_countries <- function(spec) {
+  return(get_species_locations(spec, "country_name"))
+}
+
+#' @inheritParams get_species_locations
+#' @describeIn get_species_locations Convenience functions to list
+#'             all available states for the given species.
+#' @export
+get_species_states <- function(spec) {
+  return(get_species_locations(spec, "state_name"))
+}
+
+#' @inheritParams get_species_locations
+#' @describeIn get_species_locations Convenience functions to list
+#'             all available countries for the given species.
+#' @export
+get_species_flyways <- function(spec) {
+  return(get_species_locations(spec, "flyway"))
 }
