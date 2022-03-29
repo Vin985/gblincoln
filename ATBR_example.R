103a07efc690a40314f41a7ea55823625d3a3313
Vin985 <sylvain.christin@gmail.com>
2022-03-28 21:11:38 -0300

    vignette + examples

--- ATBR_example.R
+++ ATBR_example.R
@@ -2,95 +2,93 @@ rm(list = ls())
 
 library(gblincoln)
 
+##############
+## Datasets ##
+##############
+
 # In-package database for banding data
-gb_banding
+gb_ATBR_banding
+
 # In-package database for recovery data
-gb_recoveries
+gb_ATBR_recoveries
+
+# In-package harvest data
+gb_ATBR_harvest
+
+# In-package reporting probabilities
+gb_reporting_probas
 
 # Conversion of Gamebirds names to R package
 gb_colnames
 
+###########################
+## Get Lincoln estimates ##
+###########################
+
 # Define a filter for Lincoln estimates
-filters <-
+filters_ATBR <-
   list(
     SPEC = "ATBR",
     b.state_name = "Nunavut",
     e.country_code = 'US',
-    r.flyway_code = 1
+    r.flyway_code = 1,
+    b.year= 2000:2019,
+    r.corrected_year=2000:2019
   )
 
+# Directly get lincoln estimates using provided datasets
+lincoln_estimates <-
+  get_lincoln_estimates(
+    filters = filters_ATBR,
+    banding_df = gb_ATBR_banding,
+    recoveries_df = gb_ATBR_recoveries,
+    harvest_df = gb_ATBR_harvest,
+    rho_df = gb_reporting_probas,
+    harvest_correction_factor = 0.61
+  )
+lincoln_estimates
 
-# Filter database to prepare for Lincoln estimates. By default, use banding dataset
-# and all bands
-filter_database(gb_banding, filters)
-# Same thing, different way to call it
-filter_database(gb_banding, filters, db_type = "b")
+# Get the direct recoveries dataframe
+dr_df <-
+  get_direct_recoveries(banding_df = gb_ATBR_banding,
+                        recoveries_df = gb_ATBR_recoveries,
+                        filters = filters_ATBR)
 
-# Recoveries
-filter_database(gb_recoveries, filters, db_type = "recoveries")
-# same thing, shortcut
-filter_database(gb_recoveries, filters, db_type = "r")
-# No gelocators
-filters_no_geo <-
-  list_update(filters, list(add_info = c(00, 01, 07), b.year = 2018:2019))
-filter_database(gb_recoveries, filters_no_geo, db_type = "recoveries")
 
-# Get all direct recoveries dataframe. Actually call the filter function on
-# Banding and recoveries dataframe
-direct_recoveries <-
-  get_direct_recoveries(filters = filters,
-                        banding_df = gb_banding,
-                        recoveries_df = gb_recoveries)
+# Get the harvest rate dataframe
+hr_df <- get_harvest_rate(df = dr_df, rho_df = gb_reporting_probas)
 
-# hr dataframe (uses the direct recoveries dataframe and apply RHO reporting
-# probabilities)
-hr_all_bands <-
-  get_harvest_rate(
-    filters = filters,
-    banding_df = gb_banding,
-    recoveries_df = gb_recoveries,
+# get the lincoln estimates using the previous dataframe
+get_lincoln_estimates(df = hr_df, harvest_df = gb_ATBR_harvest)
+
+
+#############################
+## Harvest rate comparison ##
+#############################
+
+filters_ATBR_all <- filters_ATBR
+# Create filter with no gelocators based on the one previously defined
+filters_ATBR_nogeo <-
+  list_update(filters_ATBR_all, list(add_info = c(00, 01, 07), b.year = 2018:2019))
+
+# Compare harvest rates
+res <-
+  compare_harvest_rates(
+    filters1 = filters_ATBR_all,
+    filters2 = filters_ATBR_nogeo,
+    banding_df = gb_ATBR_banding,
+    recoveries_df = gb_ATBR_recoveries,
     rho_df = gb_reporting_probas
   )
-# Same thing using a direct recoveries data frame instead
-hr_all_bands2 <-
-  get_harvest_rate(direct_recoveries, rho_df = gb_reporting_probas)
+# res checks if the confidence levels of the harvest rates overlap for all values
+res # All confidence levels overlap, so there should not be a problem to use geolocators
 
-# Same with No geolocator
-hr_no_geo <-
-  get_harvest_rate(
-    filters = filters_no_geo,
-    banding_df = gb_banding,
-    recoveries_df = gb_recoveries,
-    rho_df = gb_reporting_probas
-  )
+###############################
+## Get Location informations ##
+###############################
 
+get_species_locations(gb_ATBR_banding, "ATBR", sort_by = "b.country_name")
 
-# Alternatively, select directly the best dataframe based on confidence levels comparison
-comparison <- compare_harvest_rates(
-  filters1 = filters,
-  filters2 = filters_no_geo,
-  banding_df = gb_banding,
-  recoveries_df = gb_recoveries,
-)
-print(comparison)
-# Best dataframe is selected
-hr_df <- hr_all_bands
-
-# Load harvest data
-ATBRharvest <-
-  read.csv(
-    "/mnt/win/dev/EC/Lincoln/data/ATBR_harvest_2000_2019(atlantic flyway).csv",
-    stringsAsFactors = FALSE
-  )
-colnames(ATBRharvest)[1] <- "b.year"
-
-# Get Lincoln estimates for the data
-get_lincoln_estimates(hr_all_bands, harvest_df = ATBR_harvest)
-
-get_lincoln_estimates(
-  harvest_df = ATBRharvest,
-  filters = filters,
-  banding_df = gb_banding,
-  recoveries_df = gb_recoveries,
-  rho_df = gb_reporting_probas
-)
+get_species_countries(gb_ATBR_banding, "ATBR", return_codes=FALSE)
+get_species_flyways(gb_ATBR_recoveries, "ATBR")
+get_species_states(gb_ATBR_banding, "ATBR", return_names = FALSE)
